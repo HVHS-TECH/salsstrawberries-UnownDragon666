6c94a360500de0c225338277a27453cb4d58c307
UnownDragon666 <143086245+UnownDragon666@users.noreply.github.com>
2025-05-19 00:31:01 +0000

    Mostly done, gotta add auto auth on submit or something...

---
+++ fb_io.mjs
@@ -0,0 +1,73 @@
+/***************************************************************/
+// fb_io.mjs
+//
+//
+/***************************************************************/
+const COL_C = 'white';	    // These two const are part of the coloured 	
+const COL_B = '#CD7F32';	//  console.log for functions scheme
+console.log(`%c  fb_io.mjs  `, `color: ${COL_C}; background-color: ${COL_B}`);
+
+// Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-database.js";
+
+// Exports
+export { fb_initialiseAndAuth, fb_write };
+
+
+/***************************************************************/
+// fb_initialiseAndAuth()
+// Called by button in index.html
+// Initialises firebase and authenticates with Google
+// Input: None
+// Output: None
+/***************************************************************/   
+function fb_initialiseAndAuth() {
+    console.log(`%c  fb_authenticateAndAuth()  `, `color: ${COL_C}; background-color: ${COL_B}`);
+
+    // Initialise firebase
+    const firebaseConfig = {
+        apiKey: "AIzaSyCkKH0pJ-Fo9axQNsBswxIwZyuruG1X6ts",
+        authDomain: "comp-2025-idrees-munshi-24d0e.firebaseapp.com",
+        databaseURL: "https://comp-2025-idrees-munshi-24d0e-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "comp-2025-idrees-munshi-24d0e",
+        storageBucket: "comp-2025-idrees-munshi-24d0e.firebasestorage.app",
+        messagingSenderId: "811934625308",
+        appId: "1:811934625308:web:a1ff1ffffdcab01bcd79d9",
+        measurementId: "G-7P3VZN9ZFD"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const db = getDatabase(app);
+
+    // Authenticate with Google
+    let auth = getAuth();
+    const provider = new GoogleAuthProvider();
+
+    provider.setCustomParameters({
+        prompt: 'select_account'
+    });
+
+    signInWithPopup(auth, provider).then((result) => {
+        console.log(result.user);
+    }).catch((error) => {
+        console.log(error)
+    });
+}
+
+/***************************************************************/
+// fb_write()
+// Called by button in index.html
+// Writes data to firebase
+// Input: _data (object) - data to write
+// Output: None
+/***************************************************************/
+function fb_write(_data) {
+    console.log(`%c  fb_write()  `, `color: ${COL_C}; background-color: ${COL_B}`);
+
+    const db = getDatabase();
+    const auth = getAuth();
+    const refPath = ref(db, 'salsStrawberrySaloon/users/' + auth.currentUser.uid);
+    set(refPath, _data);
+}
--- main.mjs
+++ main.mjs
@@ -1,1 +1,29 @@
+/***************************************************************/
+// main.mjs
+//
+//
+/***************************************************************/
+const COL_C = 'white';	    // These two const are part of the coloured 	
+const COL_B = '#CD7F32';	//  console.log for functions scheme
+console.log(`%c  main.mjs  `, `color: ${COL_C}; background-color: ${COL_B}`);
 
+import {
+    fb_initialiseAndAuth, fb_write
+}
+    from './fb_io.mjs';
+
+window.auth = fb_initialiseAndAuth;
+
+window.submit = () => {
+    // Check if user is logged in and firebase is initialised
+    // if (!) {
+    //     fb_initialiseAndAuth();
+    // }
+    fb_write({
+        name: document.getElementById('name').value,
+        favoriteFruit: document.getElementById('favoriteFruit').value,
+        fruitQuantity: parseInt(document.getElementById('fruitQuantity').value),
+        age: parseInt(document.getElementById('age').value),
+        gender: document.getElementById('gender').value
+    });
+}
